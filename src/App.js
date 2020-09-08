6a227652a1ea717368839900f3332404c72cfcfb
pathas <pathas@naver.com>
2020-09-09 01:06:43 +0900

    Docker-Compose, Add test container

--- src/App.js
+++ src/App.js
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>우오오오오오오 오 신기해 짱 신기해</p>
+        <p>Welcome</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          도커를 학습하고 있다 리액트도 학습하고 있다
+          React-Docker
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,29 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/React-Docker/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/React-Docker/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/React-Docker/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/React-Docker/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/React-Docker/i);
   expect(linkElement).toBeInTheDocument();
 });
