7391adac44aecf4827958d02f9f6ce199cd0c79c
pathas <pathas@naver.com>
2020-09-08 18:45:05 +0900

    Docker setting...

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>우오오오오오오 오 신기해 짱 신기해</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          도커를 학습하고 있다 리액트도 학습하고 있다
         </a>
       </header>
     </div>
